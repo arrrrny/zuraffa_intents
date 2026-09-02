c7caaffa57e947985d7ba9974e9c51d46c096510
ARRRRNY <ahmettok@gmail.com>
2026-09-02 12:04:14 +0300

    Initial commit (zfa setup)

---
+++ example/zuraffa_intents_example.dart
@@ -0,0 +1,6 @@
+import 'package:zuraffa_intents/zuraffa_intents.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/zuraffa_intents_test.dart
@@ -0,0 +1,16 @@
+import 'package:zuraffa_intents/zuraffa_intents.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
